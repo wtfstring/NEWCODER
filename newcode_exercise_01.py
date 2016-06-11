68797783bd6ad289388800f6b7808148801fcccc
wangtuanfei <1437366527@qq.com>
2016-06-11 16:51:15 +0800

    PyCharm配置GITHUB

---
+++ newcode_exercise_01.py
@@ -0,0 +1,23 @@
+#coding:utf-8
+print "hello newcoder!"
+'''
+关于PyCharm配置GitHub：
+打开file，选择settings，找到Version Contorl，打开
+找到GitHub ，HOST填github.com，用户名，密码，test，稍等一会，会提示成功
+设置好后选择git，这里是输入你的git.exe的,下面是我的git.exe的位置：
+C:\Program Files\Git\cmd\git.exe
+设置好以后
+打开VCS菜单
+找到checkout from version control
+点击GitHub
+打开以后就可以发现有三个输入框，第一个是你在GitHub上创建的项目文件夹（我的是NEWCODER）
+第二个是你想要放置NEWCODER的位置（也就是NEWCODER会下载到这个地方）
+第三个是文件夹名称
+点击Clone,用过GitHub的肯定知道，这个就是把你在GIT上创建的项目文件夹同步到本地
+稍等一会儿，你会发现你的PyCharm的项目栏中多了一个文件夹，我的是NEWCODER
+现在我就可以在NEWCODER里面创建项目进行操作了
+创建新的文档以后文档以后就可以保存提交了
+暂时还没研究出怎么提交，但是在右上角有两个按钮，
+都是VSC，一个向上箭头一个向下箭头，向上的就是提交了
+后面的就交给你自己了
+'''
